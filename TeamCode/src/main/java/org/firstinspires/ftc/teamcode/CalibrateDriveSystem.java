85941acf5a4130d3b99ce01307fe0d78db274131
Rahul Ramkumar <rahulram226@gmail.com>
2017-10-31 19:07:52 -0400

    Add Test For Drive System

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrateDriveSystem.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+public class CalibrateDriveSystem extends rr_OpMode {
+    rr_Robot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new rr_Robot(this, this.hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            robot.universalMoveRobot(this, gamepad1.left_stick_x, gamepad1.left_stick_y);
+        }
+    }
+}
