83e21cfce5f88901ffce4d179da17a8cce5d3bc6
Rahul Ramkumar <rahulram226@gmail.com>
2017-11-26 16:20:20 -0500

    Add Motor Tests
    for base wheels, cube arm, and relic winch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr_DiagLib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr_DiagLib.java
@@ -1,8 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import static org.firstinspires.ftc.teamcode.rr_Constants.BACK_LEFT_MOTOR;
+import static org.firstinspires.ftc.teamcode.rr_Constants.BACK_RIGHT_MOTOR;
+import static org.firstinspires.ftc.teamcode.rr_Constants.CUBE_ARM;
+import static org.firstinspires.ftc.teamcode.rr_Constants.FRONT_LEFT_MOTOR;
+import static org.firstinspires.ftc.teamcode.rr_Constants.FRONT_RIGHT_MOTOR;
+import static org.firstinspires.ftc.teamcode.rr_Constants.RELIC_WINCH;
+
 public class rr_DiagLib {
+
+    private final static float MECCANUM_WHEEL_ENCODER_MARGIN = 50;
 
     rr_Robot robot;
     rr_OpMode aOpMode;
@@ -10,5 +20,42 @@ public class rr_DiagLib {
     public rr_DiagLib(rr_OpMode aOpMode, HardwareMap aHwMap) throws InterruptedException {
         robot = new rr_Robot(aOpMode, aHwMap);
         this.aOpMode = aOpMode;
+    }
+
+
+    //***************** MOTOR TESTS *****************//
+
+    private boolean genericMotorTest(int motor) throws InterruptedException {
+        robot.setMotorMode(aOpMode, motor, DcMotor.RunMode.RUN_USING_ENCODER);
+        int motorPosition = robot.getMotorPosition(aOpMode, motor);
+        robot.testMotor(aOpMode, motor, 0.5f, 1000);    // Runs motor for 1 second at half speed
+        int newMotorPosition = robot.getMotorPosition(aOpMode, motor);
+
+        // If the positions are different enough, the motor is running and working
+        return !(Math.abs(newMotorPosition - motorPosition) < MECCANUM_WHEEL_ENCODER_MARGIN);
+    }
+
+    public boolean testFrontLeftWheel() throws InterruptedException {
+        return genericMotorTest(FRONT_LEFT_MOTOR);
+    }
+
+    public boolean testFrontRightWheel() throws InterruptedException {
+        return genericMotorTest(FRONT_RIGHT_MOTOR);
+    }
+
+    public boolean testBackLeftWheel() throws InterruptedException {
+        return genericMotorTest(BACK_LEFT_MOTOR);
+    }
+
+    public boolean testBackRightWheel() throws InterruptedException {
+        return genericMotorTest(BACK_RIGHT_MOTOR);
+    }
+
+    public boolean testCubeArmMotor() throws InterruptedException {
+        return genericMotorTest(CUBE_ARM);
+    }
+
+    public boolean testRelicWinchMotor() throws InterruptedException {
+        return genericMotorTest(RELIC_WINCH);
     }
 }
